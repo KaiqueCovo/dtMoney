33ea1a561dde4b07c791b1dfbedcf16ca4701010
Kaique Covo <kaique.kng@gmail.com>
2022-01-09 18:58:36 -0300

    fix: Import from App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "../src/hooks/transactions";
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
